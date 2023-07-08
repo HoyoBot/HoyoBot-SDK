cce62333c6073ba93898f062617a0e99d8beb1c6
Catrainbow <2496442208@qq.com>
2023-07-08 14:49:10 +0800

    init main

--- sdk-main/src/main/kotlin/cn/hoyobot/sdk/HoyoBot.kt
+++ sdk-main/src/main/kotlin/cn/hoyobot/sdk/HoyoBot.kt
@@ -1,4 +1,30 @@
 package cn.hoyobot.sdk
 
+import cn.hutool.log.Log
+import cn.hutool.log.LogFactory
+import lombok.Getter
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+@Getter
 class HoyoBot {
+
+    companion object {
+        lateinit var instance: HoyoBot;
+    }
+
+    private val version = "1.0.0"
+    private val path = System.getProperty("user.dir") + "/"
+    private val pluginPath = path + "plugins"
+    private val logger: Log = LogFactory.get("HoyoBot");
+
+    fun main(args: Array<String>) {
+        instance = this;
+        this.logger.info("HoyoBot - ${this.version}")
+        this.logger.info("Find updates at: https://github.com/HoyoBot/HoyoBot-SDK")
+        //TODO: Plugin Loader
+        SpringApplication.run(this.javaClass)
+    }
+
 }
